8a74564f3c322be4ab2806c453c326daa9bf800d
sadaf <enduser@Sadafs-MacBook-Pro.local>
2026-06-25 22:39:55 +0430

    Fix Streamlit deployment TensorFlow + Keras import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from labels import labels
 
 # =========================
